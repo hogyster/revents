99448f7c6a7f6975b9c8cd29d0897d5db29d3d56
unknown <hogister@naver.com>
2018-12-04 22:48:54 +0900

    End of SEction 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router-dom";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import EventForm from "../../features/event/EventForm/EventForm";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import HomePage from "../../features/home/HomePage";
+
 // our component is javascript class that extends from react component and our component require single method called render, which then returns look like html rendered on the page (which is javascript, the jsx) similar to html but get compiled to javascript and injected to our html page. we use web packs behind the scenes, hot module replacement- make changes to our code and reflected on the page without refreshing.
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                  {/* when we use route comp, will load the component we specify and replace the routes with that particular component, need route for home components*/}
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,36 +1,68 @@
 import React, { Component } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
+
+// NavLink- active
+// use Link - when something does not show active automatically
 class NavBar extends Component {
+  state = {
+    authenticated: false
+  };
+
+  handleSignIn = () => {
+    this.setState({
+      authenticated: true
+    });
+  };
+
+  handleSignOut = () => {
+    this.setState({
+      authenticated: false
+    });
+    this.props.history.push("/");
+  };
+
+  // withouth authentication boolean- to check if user is loged in
   render() {
+    const { authenticated } = this.state;
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
-            <img src="assets/logo.png" alt="logo" />
+          <Menu.Item as={Link} to="/" header>
+            <img src="/assets/logo.png" alt="logo" />
+            {/* need forward slash to have is as absolute path path to our logo */}
             re-vents
           </Menu.Item>
-          <Menu.Item name="Request" />
-          <Menu.Item>
-            <Button
-              floated="right"
-              positive
-              inverted
-              content="Create Request"
-            />
-          </Menu.Item>
-          <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button
-              basic
-              inverted
-              content="Sign Out"
-              style={{ marginLeft: "0.5em" }}
-            />
-          </Menu.Item>
+          {/* this means that this menu as= item is now NavLink so any properties that is passed to the navlink, we can pass in here and added to this particular component*/}
+          <Menu.Item as={NavLink} to="/events" name="Events" />
+          {authenticated && (
+            <Menu.Item as={NavLink} to="/people" name="People" />
+          )}
+          {/* can't wrap to jsons, jsx menu in a single expression  */}
+          {authenticated && (
+            <Menu.Item>
+              <Button
+                as={Link}
+                to="/createEvent"
+                floated="right"
+                positive
+                inverted
+                content="Create Request"
+              />
+            </Menu.Item>
+          )}
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} />
+          )}
         </Container>
       </Menu>
     );
   }
 }
 
-export default NavBar;
+// Pass a NavBar as a parameter to the withRouter function and when this function executes, will return a new component(navbar), which is react component - with routing functionality - they take comp as parameter and return with additional features
+export default withRouter(NavBar);
